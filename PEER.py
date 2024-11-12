75298470fdcd713be70bf790bdf1378cee4f563a
ReneRevay <renerevay57@gmail.com>
2024-11-12 17:20:12 +0100

    New version. Remade it but has the same functionality. Ended At hand shake for now.

--- PEER.py
+++ PEER.py
@@ -1,145 +1,152 @@
 import socket
 import threading
 import struct
-import time
-import random
+import os
+from flags import Flags
+from binascii import crc_hqx
+"""
+Flags:
+SYN =                0b00000001
+ACK =                0b00000010
+NACK =               0b00000100
+KILL =               0b00001000
+KEEP_ALIVE =         0b00010000
+SENDING_TEXT =       0b00100000
+SENDING_FILE =       0b00100001
+LAST_TEXT_FRAGMENT = 0b10000000
+LAST_FILE_FRAGMENT = 0b10000001
+"""
 
-# FLAGS:
-# S - syn  -  handshake
-# A - ack  -  handshake
-# V - verify   -  keep-alive
-# L - last fragment  -  recieve segments until this flag arrives
-# K - kill connection  -  for ending and closing connections between peers
-# C - corrupted  -  if file arrives damaged, respond with this flag and request that flag again
-# F - sending file  -  data is to be interpreted as a file
-# T - sending text  -  data is to be interpreted as text
-
-# * Add full functionality of seq and ack nums
-# TODO implement kill connection flag to cancel the program like normal human beeing and not killing it every time 
-# TODO implement keep alive 
-# TODO implement crc16
-
+#----------------------INTERNET / AI GENERATED---------------------------------------------------
 
 def get_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
-        s.connect(('10.255.255.255', 1))  # random IP just to send a packet
-        IP = s.getsockname()[0]
+        s.connect(('10.255.255.255', 1))   # random IP just to send a packet
+        IP = s.getsockname()[0]   # get our actual ip
     except:
         IP = '127.0.0.1'
     finally:
         s.close()
     return IP
 
+#--------------------------------------------------------------------------------------------------
+
 class PEER:
-    def __init__(self, local_ip, reciever_port, sender_port, dest_ip, dest_port):
+    def __init__(self, local_ip, local_port, dest_ip, dest_port):
         self.local_ip = local_ip
-        self.reciever_port = reciever_port
-        self.sender_port = sender_port
+        self.local_port = local_port
         self.destination_ip = dest_ip
         self.destination_port = dest_port
 
-        self.SYN  =  0b10000000
-        self.ACK  =  0b01000000
-        self.VER  =  0b00100000
-        self.LAST =  0b00010000
-        self.KILL =  0b00001000
-        self.COR  =  0b00000100
-        self.FILE =  0b00000010
-        self.TEXT =  0b00000001
+        self.done_handshake = False
+        self.MAX_FRAGMENT_SIZE = 1465
 
         self.send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.recv_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.send_sock.bind((self.local_ip, self.sender_port))
-        self.recv_sock.bind((self.local_ip, self.reciever_port))
+        self.recv_sock.bind((self.local_ip, self.local_port))
 
-    def create_header(self, seq_num, ack_num, batch, sum, flags):
-        return struct.pack('!HHBHB', seq_num, ack_num, batch, sum, flags)
 
-    def send_message(self, seq_num, ack_num, batch, sum, flags, data):
-        header = self.create_header(seq_num, ack_num, batch, sum, flags)
-        segment = header + data.encode('utf-8')
+    def create_header(self, seq_num, crc16, flags):
+        return struct.pack('!IHB', seq_num, crc16, flags)
+
+    def send_message(self, seq_num, crc16, flags, data):
+        header = self.create_header(seq_num, crc16, flags)
+        if data == None: segment = header 
+        else: segment = header + data.encode('utf-8')
         self.send_sock.sendto(segment, (self.destination_ip, self.destination_port))
 
-    def sender(self, data):
-        print("starting sender")
-        done_handshake = False
+    def send_response(self, seq_num, crc16, flag, client):
+        header = self.create_header(seq_num, crc16, flag)
+        self.recv_sock.sendto(header, client)
 
-        seq_num = random.randint(0,128)
-        ack_num = 0
-        batch = 1
-        crc16 = 0
-        
-        if not done_handshake:
-            while True:
-                print("HandShake not done, Let's do it! Sending SYN.")
-                header = self.create_header(seq_num, ack_num, batch, crc16, self.SYN)
-                self.send_sock.sendto(header, (self.destination_ip, self.destination_port))
-                try:
-                    self.send_sock.settimeout(1.0) # Waiting a specific amount of time to get ACK
-                    whole_data, client = self.send_sock.recvfrom(1024)
-                    header = whole_data[:8]
-                    seq_num, ack_num, batch, crc16, self.flags = struct.unpack("!HHBHB", header)
-                    seq_num = ack_num
-                    ack_num = 0
-                    if self.flags == self.ACK:
-                        print("ACK arrived, it's done!")
-                        self.done_handshake = True
-                        break
-                except socket.timeout:
-                    print("ACK didn't arrive in time!")
-                    continue
+    def establish_connection(self):
+        number_of_tries = 0
+        while number_of_tries < 3:
+            print("Establishing connection!\nStarting HandShake!\n")
+            self.send_message(0, 0, Flags.SYN, None)
+            try:
+                self.send_sock.settimeout(1.0) # waiting one second for ack 
+                whole_data, _ = self.send_sock.recvfrom(self.MAX_FRAGMENT_SIZE)
+                header = whole_data[:7] 
+                _, _, flag = struct.unpack("!IHB", header)
+                if flag == Flags.ACK:
+                    self.done_handshake = True
+                    break
+                
+            except socket.timeout:
+                number_of_tries += 1
+                continue
 
-        while data != "end":
-            self.send_message(seq_num,ack_num,batch,crc16,self.LAST,data) #for now there is LAST flag as a default
-            # add a portion of code to recieve a ack response from sender and print out the content of header, this may be the fix for the not changing seq and ack nums
-            data = input("Data to be sent: ")
-        self.send_sock.close()
-        self.recv_sock.close()
 
     def receiver(self):
         print("Starting receiver")
-
-        seq_num = 0
-        ack_num = 0
-        batch = 1
-        crc16 = 0
-
+        arrived = 0
         while True:
-            whole_data, client = self.recv_sock.recvfrom(1024)
-            header = whole_data[:8]
-            seq_num, ack_num, batch, crc16, self.flags = struct.unpack("!HHBHB", header)
-            ack_num = seq_num + 1
-            seq_num = 0
-            data = whole_data[8:]
-
-            if self.flags == self.SYN:
-                print("SYN arrived for handshake, sending ACK")
-                header = self.create_header(seq_num, ack_num, batch, crc16, self.ACK)
-                self.recv_sock.sendto(header, client)
+            whole_data, client = self.recv_sock.recvfrom(self.MAX_FRAGMENT_SIZE)
+            header = whole_data[:7]
+            seq_num, crc16, flag = struct.unpack("!IHB", header)
+            data = whole_data[7:]
+            
+            if flag == Flags.SYN:
+                arrived += 1
+                if arrived == 2: 
+                    self.send_response(0, 0, Flags.ACK, client)
 
             print(f"Received message: {data.decode()} from {client}")
-            print(f"Header details : seq_num: {seq_num}, ack_num: {ack_num}, batch: {batch}, sum: {crc16}, flags: {self.flags}")
+            print(f"Header details : seq_num: {seq_num}, sum: {crc16}, flags: {flag}")
+
+
+    def sender(self):
+        print("Starting sender")
+        
+        seq_num = 0
+
+        while True:
+            user_input = input("\nDo you want to send something?\n-yes/y : continue to specify your sending parameters \n-exit/e : quit this application\n")
+            if user_input == "exit" or user_input == 'e':
+                break
+            elif user_input == "yes" or user_input == 'y':
+                if not self.done_handshake:
+                    self.establish_connection()
+                else:
+                    user_input = input("Input the desired size of fragment: ")
+                    if user_input.isnumeric() and  int(user_input) <= self.MAX_FRAGMENT_SIZE: 
+                        fragment_size = user_input
+                    else:
+                        continue
+
+                    user_input = input("Do you want to send a text or a file (t/f): ")
+                    if user_input == 't':
+                        message = input("Input you text that you want to send: ")
+                        self.send_message(seq_num, 0, Flags.SENDING_TEXT, message)
+                        seq_num += 1
+
+                    elif user_input == 'f':
+                        print("File functionality will be done later")
+                    else: 
+                        continue
+            else: 
+                continue
+            
+        os._exit(1)
 
 
     def begin(self):
-        receiver = threading.Thread(target=self.receiver,daemon=True)
+        receiver = threading.Thread(target=self.receiver, daemon=True)
         receiver.start()
-        
-        data = input("Input data to be sent: ")
-        if data:
-            sender = threading.Thread(target=self.sender, args=(data,),daemon=True)
-            sender.start()
-            sender.join()
-        
+
+        sender = threading.Thread(target=self.sender, daemon=True)
+        sender.start()
+
+        sender.join()
         receiver.join()
 
 if __name__ == "__main__":
     #local_ip = get_ip()
     local_ip = "127.0.0.1"
-    reciever_port = int(input("Input the port you are listening on: "))
-    sender_port = int(input("Enter port, from which you will be sending: "))
+    local_port = int(input("Input the port you are listening on: "))
     destination_ip = input("Input the IP of the host: ") or "127.0.0.1"
     destination_port = int(input("Input the host's reciever port: "))
-    peer = PEER(local_ip, reciever_port, sender_port, destination_ip, destination_port)
+    peer = PEER(local_ip, local_port, destination_ip, destination_port)
     peer.begin()
---
+++ flags.py
@@ -0,0 +1,10 @@
+class Flags:
+    SYN =                0b00000001
+    ACK =                0b00000010
+    NACK =               0b00000100
+    KILL =               0b00001000
+    KEEP_ALIVE =         0b00010000
+    SENDING_TEXT =       0b00100000
+    SENDING_FILE =       0b00100001
+    LAST_TEXT_FRAGMENT = 0b10000000
+    LAST_FILE_FRAGMENT = 0b10000001
